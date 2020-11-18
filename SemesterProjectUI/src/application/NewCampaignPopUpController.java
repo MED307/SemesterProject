991bd6b54e5ddf12bc597ff40a4d2cbf64d0ea00
absolutelunacy <ulrik@kampel.dk>
2020-11-18 12:47:18 +0100

    Very small update

--- SemesterProjectUI/src/application/NewCampaignPopUpController.java
+++ SemesterProjectUI/src/application/NewCampaignPopUpController.java
@@ -1,7 +1,28 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
 public class NewCampaignPopUpController extends Controller {
 
 	
+	//FXML imports
+	@FXML
+	private TextField campaignNameTextField;
+	
+	@FXML
+	private Button createCampaignBtn;
+	
+	
+	public void campaignButton(ActionEvent event) 
+	{
+		
+			
+		
+	}
 	
 }
